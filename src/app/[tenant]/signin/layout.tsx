0dabc3d8d020f9b2f7b640ee66d8fb1b5ac2407c
hcorredor <hcorredor@novopayment.com>
2023-08-03 15:29:41 -0500

    Incorpora layout de signin

---
+++ src/app/[tenant]/signin/layout.tsx
@@ -0,0 +1,12 @@
+import { RootLayout } from '@/interfaces';
+import { Metadata } from 'next';
+import React from 'react';
+
+export const metadata: Metadata = {
+  title: 'Signin - Admin Console',
+  description: '...',
+};
+
+export default function Signinlayout({ children }: RootLayout) {
+  return <div>{children}</div>;
+}
