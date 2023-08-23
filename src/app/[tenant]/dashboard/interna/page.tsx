ca8786544abd52319acab26785f4249864339230
ylopez <ylopez@novopayment.com>
2023-08-23 08:57:21 -0500

    Incopora nuevas paginas

---
+++ src/app/[tenant]/dashboard/interna/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Internal Page - Admin Console",
+  description: "Your profile page",
+};
+
+export default function Interna() {
+
+	return (
+		<> Pagina de interna </>
+	)
+}
