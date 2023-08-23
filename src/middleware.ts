d73729b430fec7d1881b2b2c4eefdcc66b6b5371
ylopez <ylopez@novopayment.com>
2023-08-23 09:01:15 -0500

    Incopora validación de sesión

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,32 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { log_message } from "@/utils";
+import { NextRequest, NextResponse } from 'next/server'
+import { getToken } from "next-auth/jwt"
+import { log_message } from "@/utils"
 
-const access_url = process.env.NEXT_PUBLIC_ACCESS_URL;
-const validTenants = access_url?.split(',');
+const access_url = process.env.NEXT_PUBLIC_ACCESS_URL
+const validTenants = access_url?.split(',')
+const defaultTenant = validTenants?.[0]
+const SIGNIN_ROUTE = '/signin'
 
-const SIGNIN_ROUTE = '/signin';
+export async function middleware(req: NextRequest) {
 
-export function middleware(req: NextRequest) {
+	console.log('HEADEERS -> ', req)
+
+
 	const url = req.nextUrl.clone();
 	const tenant = getPathName(url);
+	const requireAuth: string[] = [`/${tenant}/dashboard`]
 
-  log_message(`middleware en url pathname: ${url.pathname}`,'','fetch')
-	const defaultTenant = validTenants?.[0];
+  log_message('debug',`middleware en url pathname: ${url.pathname}`,'fetch')
 
 	if (!validTenants?.includes(tenant)) {
 		//Redirect Tenant default
-		return redirectTo(url, `/${defaultTenant}${SIGNIN_ROUTE}`);
+		return redirectTo(url, `/${defaultTenant}${SIGNIN_ROUTE}`, [])
 	}
+
+	if (requireAuth.some((path) => url.pathname.startsWith(path))) {
+		return validateSession(url, tenant, req)
+	}
+
 }
 
 function getPathName(url: URL): string {
@@ -24,11 +34,31 @@ function getPathName(url: URL): string {
 	return pathname;
 }
 
-function redirectTo(url: URL, path: string): NextResponse {
+function redirectTo(url: URL, path: string, cookies: string[] = []): NextResponse {
+
 	url.pathname = path;
-	return NextResponse.redirect(url);
+	const response = NextResponse.redirect(url);
+
+	if (cookies.length > 0) {
+		cookies.forEach((element: any) => {
+			response.cookies.set(element, '', { expires: new Date(Date.now()) });
+		});
+	}
+
+	return response
+}
+
+async function validateSession(url: URL, tenant:string, req:any) {
+
+	const ip = req.headers.get('x-forwarded-for');
+	const token: any = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
+	console.log('ES EL TOKEN NUEVO ', token)
+
+	if (!token || (token.ip != ip)) {
+		return redirectTo(url,`/${tenant}${SIGNIN_ROUTE}`,['next-auth.session-token'])
+	}
 }
 
 export const config = {
-	matcher: ['/:tenant/(signin)'],
+	matcher: ['/:tenant/signin', '/:tenant/dashboard', '/:tenant/dashboard/:path*']
 };
