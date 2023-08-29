096b2abbfe1378830c5e211922ad604701f8d1ed
ylopez <ylopez@novopayment.com>
2023-08-29 11:08:40 -0500

    Incorpora persitir data Zustand

--- src/app/[tenant]/dashboard/interna/page.tsx
+++ src/app/[tenant]/dashboard/interna/page.tsx
@@ -1,3 +1,9 @@
+'use client'
+
+import useGetFormStore from '@/hooks/zustanHooks';
+import { useCountStore } from '@/store/useCountStore';
+
+
 export const metadata = {
   title: "Internal Page - Admin Console",
   description: "Your profile page",
@@ -5,7 +11,13 @@ export const metadata = {
 
 export default function Interna() {
 
+	const { addCount } = useCountStore();
+	const bears = useGetFormStore(useCountStore, (state) => state.count)
 	return (
-		<> Pagina de interna </>
+		<>
+			Pagina de interna {bears}
+			<button onClick={addCount}>Agregar</button>
+		</>
+
 	)
 }
---
+++ src/store/useCountStore.ts
@@ -0,0 +1,27 @@
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+
+type Store = {
+	count: number
+}
+
+type Actions = {
+	addCount: () => void;
+	removeCount: () => void;
+	resetCount: () => void;
+}
+
+// the store itself does not need any change
+export const useCountStore = create<Store & Actions>() (
+  persist(
+    (set) => ({
+			count: 1,
+			addCount: () => set((state: any) => ({ count: state.count + 1 })),
+			removeCount: () => set((state: any) => ({ count: state.count - 1 })),
+			resetCount: () => set(() => ({ count: 0 }))
+    }),
+    {
+      name: 'global'
+    }
+  )
+)
