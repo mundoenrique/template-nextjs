9972cb04ce7bf79fc06fb14f1079809e360138e1
ylopez <ylopez@novopayment.com>
2023-08-29 10:47:16 -0500

    Ajusta conexión API

--- src/services/connectApi.ts
+++ src/services/connectApi.ts
@@ -1,14 +1,29 @@
 
 import axios, { AxiosInstance, AxiosError } from 'axios';
 
+let client: boolean = false
+if (typeof window !== "undefined") {
+	client = true
+}
+
 const connectApi: AxiosInstance = axios.create({
-  baseURL: '/api'
+	baseURL: client ? '/api' : process.env.NEXT_PUBLIC_PATH_URL + '/api',
+	timeout: 50000
 });
+
+connectApi.interceptors.request.use(
+	(config) => {
+		const timeout = 50000
+		config.timeout = timeout;
+		return config
+	},
+	(err) => Error(err)
+)
 
 // Interceptor para manejar errores de respuesta
 connectApi.interceptors.response.use(
   (response) => response.data,
-  (error: AxiosError) => {
+	(error: AxiosError) => {
       return Promise.resolve({
         code: -1,
         data: 'At this time we are unable to accommodate your request, please try again later.',
