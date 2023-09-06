62dc06e64092c60e0678fbc4220bf245026e69d6
ylopez <ylopez@novopayment.com>
2023-09-06 11:15:40 -0500

    Incopora llamado registro cliente en Redis

--- src/app/[tenant]/signin/page.tsx
+++ src/app/[tenant]/signin/page.tsx
@@ -1,20 +1,23 @@
 'use client';
 
 import { signIn } from "next-auth/react"
-import { useSession } from "next-auth/react"
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { Box, Typography, Grid, Button } from '@mui/material';
 import { useRouter } from 'next/navigation'
+import { useEffect, useState } from "react";
 //Internal App
 import { log_message } from "@/utils";
 import { useLangStore } from '@/store/langStore';
 import { useTranslation } from '@/app/i18n/client';
 import { getSchema } from '@/config/validationConfig';
+import connectApi from "@/services/connectApi";
+import useGetFormStore from "@/hooks/zustanHooks";
 import {
 	InputPass,
 	InputText,
 	NavBar,
+	Modals
 } from '@/components/UI';
 
 type FormData = {
@@ -22,14 +25,33 @@ type FormData = {
 	password: string
 }
 
+type resData = {
+	code: number,
+	msg: string
+}
+
+const sesionClient = async () => {
+	const res:resData = await connectApi.get(`/redisSesion`)
+	return res
+}
+
 export default function LoginPage({ params }: any) {
 
-	const { data: session, status } = useSession()
+	const [showModal, setShowModal] = useState(false);
+
+	useEffect(() => {
+		sesionClient().then((data) => {
+			if (data.code != 0) {
+				setShowModal(true)
+			}
+    });
+  }, []);
+
 	const router = useRouter()
 
 	log_message('info','Accede a la pagin a SIG-IN')
-	const { lang } = useLangStore();
-	const { t } = useTranslation(lang, `${params.tenant}-general`);
+	const lang = useGetFormStore(useLangStore, (state) => state.lang)
+	const { t } = useTranslation(lang!, `${params.tenant}-general`);
 	const schema = getSchema([
 		'email',
 		'password'
@@ -54,12 +76,10 @@ export default function LoginPage({ params }: any) {
 		if (nuevo?.error === null) {
 			router.push(`dashboard`)
 		}
-
 	}
 
 	return (
 		<>
-			Hi {JSON.stringify(session)} estado { status }!
 			<NavBar />
 
 			<Box sx={{ m: 5 }} component="form" onSubmit={handleSubmit(onLoginUser)}>
@@ -88,6 +108,19 @@ export default function LoginPage({ params }: any) {
 					</Grid>
 				</Grid>
 			</Box>
+
+			<Modals
+				msgModal={'En este momento no podemos procesar tu solicitud'}
+				buttons={1}
+				showModal={showModal}
+			>
+				<Button
+					variant="contained"
+					onClick={() => { setShowModal(false) }}
+				>
+					{t('buttons.accept')}
+				</Button>
+			</Modals>
 		</>
 	);
 }
