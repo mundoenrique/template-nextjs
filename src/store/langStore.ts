dbd24f619979080ba7ae9fd1c0ebe9120729ab6c
ylopez <ylopez@novopayment.com>
2023-09-06 10:49:45 -0500

    Ajusta store lenguaje

--- src/store/langStore.ts
+++ src/store/langStore.ts
@@ -1,18 +1,19 @@
-import Cookies from 'js-cookie'
-import {create} from 'zustand'
-import { log_message } from "@/utils";
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+
 interface LangState {
 lang: string
 changeLang: (lang: string) => void
 }
 
-const defaultLang = Cookies.get('i18Next') || 'es';
-
-export const useLangStore = create<LangState>((set) => ({
-  lang: defaultLang,
-  changeLang: (lang:string) => {
-   log_message('debug', `Realiza cambio de idioma - ${lang}`)
-    Cookies.set('i18Next', lang)
-    set({lang})
-  }
-}))
+export const useLangStore = create<LangState>()(
+	persist(
+	(set) => ({
+		lang: 'es',
+		changeLang: (lang) => set(() => ({ lang: lang })),
+		}),
+		{
+      name: 'languaje'
+    }
+	)
+)
