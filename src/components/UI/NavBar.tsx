0489601f3e05a10613d72c721577c25ae42a11fe
ylopez <ylopez@novopayment.com>
2023-09-07 09:04:51 -0500

    Incopora llamado de sesion

--- src/components/UI/NavBar.tsx
+++ src/components/UI/NavBar.tsx
@@ -1,17 +1,23 @@
 import Image from 'next/image';
+import { useSession } from "next-auth/react"
 import { usePathname } from 'next/navigation';
-import { AppBar, Container, Toolbar } from '@mui/material';
+import { AppBar, Container, Toolbar, Typography } from '@mui/material';
 
 export default function NavBar(): JSX.Element {
+
+	const { data: session } = useSession()
   const router = usePathname();
   const currentTenant = router.split('/')[1] || 'novo';
 
   return (
     <AppBar position='static'>
-      <Container maxWidth='xl'>
+      <Container maxWidth='xl' sx={{ textAlign:'center'}}>
         <Toolbar disableGutters>
-          <Image src={`/images/${currentTenant}/img-logo-color.svg`} fill alt='Picture of the author' priority />
-        </Toolbar>
+					<Image src={`/images/${currentTenant}/img-logo-color.svg`} fill alt='Picture of the author' priority />
+				</Toolbar>
+				<Typography variant="h6" color="primary" component="div">
+         { session ? `Bienvenido ${session?.user?.name}` : ''}
+        </Typography>
       </Container>
     </AppBar>
   );
