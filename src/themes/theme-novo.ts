2315bb51a130828c568230288e26ac657942c914
ylopez <ylopez@novopayment.com>
2023-10-05 14:23:27 -0500

    Incorpora Google Recaptcha

--- src/themes/theme-novo.ts
+++ src/themes/theme-novo.ts
@@ -91,7 +91,8 @@ export function changeMode(mode: string, count: number) {
         },
       },
       // Global component styles
-      MuiCssBaseline: {
+			MuiCssBaseline: {
+
         styleOverrides: `
           @font-face {
             font-family: 'Prelo';
@@ -109,6 +110,16 @@ export function changeMode(mode: string, count: number) {
             src: local('Prelo'), local('Prelo-Bold'), url(/fonts/novo/prelo-semibold-webfont.woff2) format('woff2');
             unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
           }
+					.grecaptcha-badge {
+						width: 70px !important;
+						overflow: hidden !important;
+						transition: all 0.3s ease !important;
+						left: -2px !important;
+						bottom: 20px !important;
+					}
+					.grecaptcha-badge:hover {
+						width: 256px !important;
+					}
         `,
       },
       // Main container styles
---
+++ src/views/Signin.tsx
@@ -0,0 +1,132 @@
+'use client';
+
+import React, { useRef, useState } from "react";
+import { signIn } from 'next-auth/react';
+import { useForm } from 'react-hook-form';
+import { useRouter } from 'next/navigation';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Box, Typography, Grid, Button, CircularProgress } from '@mui/material';
+import {
+	InputPass,
+	InputText,
+	NavBar,
+	Modals,
+	Cookies
+} from '@/components/UI';
+import { useGoogleReCaptcha } from "react-google-recaptcha-v3"
+const RecaptchaPuzzle:any = dynamic(() => import('@/components/UI/RecaptchaPuzzle'));
+
+//Internal App
+import { getSchema } from '@/config';
+import { log_message } from '@/utils';
+import connectApi from '@/services/connectApi';
+import { useTranslation } from '@/app/i18n/client';
+import dynamic from "next/dynamic";
+
+export default function Signin({ tenant }: any) {
+
+	const { executeRecaptcha } = useGoogleReCaptcha()
+  const [showModal, setShowModal] = useState(false);
+  const [showPuzzle, setShowPuzzle] = useState(false);
+	const [loading, setLoading] = useState(false);
+	const [msgModal, setmsgModal] = useState('');
+
+  log_message('info', 'Access the SIG-IN page');
+  const router = useRouter();
+  const { t } = useTranslation();
+  const schema = getSchema(['email', 'password'], tenant); //currentTenant
+
+  type FormData = {
+    email: string;
+    password: string;
+  };
+
+  type resData = {
+    code: number;
+    payload: object;
+  };
+
+  const { handleSubmit, control, reset } = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    resolver: yupResolver(schema)
+	});
+
+	const sesionClient = async ({ email, password }: FormData) => {
+
+		setLoading(true)
+		const res: resData = await connectApi.get(`/redisSesion`);
+		if (res.code === 0) {
+			await onLoginUser({email,password})
+		} else {
+			setmsgModal('We are unable to process your request at this time')
+			setLoading(false)
+			setShowModal(true)
+		}
+    return res;
+  };
+
+	const onLoginUser = async ({ email, password }: FormData) => {
+
+		if (!executeRecaptcha) {
+      return
+    }
+
+		const token = await executeRecaptcha("onSubmit")
+		const {code, payload}:resData = await connectApi.post(`/recaptchaVerify`, {
+			data: { token }
+		});
+		console.log('DATA VERIFYRECAPTCHA ', payload)
+
+		const resLogin = await signIn('credentials', { redirect: false, email, password })
+		if (resLogin?.error === null) {
+			const date = new Date
+			localStorage.setItem('sessionTime', date.toString())
+			router.push(`dashboard`);
+		} else {
+			setmsgModal('Invalid username or password. Please try again.')
+			setShowModal(true);
+		}
+  };
+
+  return (
+		<>
+			<Cookies />
+      <NavBar />
+
+      <Box sx={{ m: 5 }} component='form' onSubmit={handleSubmit(sesionClient)}>
+        <Typography variant='h3' sx={{ mb: 3 }}>
+          Sign-in
+        </Typography>
+        <Grid container columns={1} spacing={2}>
+          <Grid item xs={2}>
+            <InputText name='email' control={control} optional />
+						<InputPass name='password' control={control} additionalInfo />
+
+						<Button variant='contained' type='submit' disabled={loading} fullWidth>
+							{loading && <CircularProgress color='secondary' size={20} />}
+              {!loading && t('buttons.accept')}
+						</Button>
+          </Grid>
+        </Grid>
+      </Box>
+
+      <Modals msgModal={msgModal} buttons={1} showModal={showModal}>
+        <Button
+          variant='contained'
+					onClick={() => {
+						setLoading(false);
+            setShowModal(false);
+          }}
+        >
+          {t('buttons.accept')}
+      	</Button>
+			</Modals>
+
+			{showPuzzle && <RecaptchaPuzzle showPuzzle={showPuzzle} setShowPuzzle={setShowPuzzle} />}
+
+    </>
+  );
+}
