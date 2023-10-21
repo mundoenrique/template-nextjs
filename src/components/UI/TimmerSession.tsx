8bdf9b9145379a0df585d4d63c46413e24c10e7c
hcorredor <hcorredor@novopayment.com>
2023-10-20 19:07:56 -0500

    Incopora eventos para el manejo de la sesion

--- src/components/UI/TimmerSession.tsx
+++ src/components/UI/TimmerSession.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Button } from '@mui/material';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useSession, signOut, getSession } from 'next-auth/react';
 //Internal app
 import { Modals } from '@/components/UI';
@@ -9,18 +9,14 @@ import { useTranslation } from '@/app/i18n/client';
 
 export default function TimmerSession() {
   const { t } = useTranslation();
-  const { data: session } = useSession();
   const [showModal, setShowModal] = useState(false);
   const [inactive, setInactive] = useState(false);
-  const { update } = useSession();
+  const { update, data: session } = useSession();
+  const ref: any = useRef(null);
 
   useEffect(() => {
     let inactiveTime: any;
     const inactiveTimeLimit = 30000;
-
-    if (!localStorage.sessionTime) {
-      signOut();
-    }
 
     const resetInactive = () => {
       clearTimeout(inactiveTime);
@@ -54,23 +50,63 @@ export default function TimmerSession() {
     };
   }, [inactive, showModal]);
 
+  useEffect(() => {
+    let intervalSession: any;
+
+    const timerSession = () => {
+      if (!localStorage.sessionTime) {
+        signOut();
+      }
+
+      const date = new Date(localStorage.sessionTime).getTime();
+      const now = new Date().getTime();
+      const time = Math.trunc(Math.abs((date - now) / 1000));
+      const timeRest = parseInt(process.env.NEXT_PUBLIC_SESS_EXPIRATION || '300') - time;
+      time.toString() === '1' && ref.current.click();
+      if (timeRest === 45) {
+        setShowModal(true);
+      } else if (timeRest <= 0) {
+        clearInterval(intervalSession);
+        localStorage.removeItem('sessionTime');
+        signOut();
+      }
+    };
+
+    intervalSession = setInterval(timerSession, 1000);
+  }, []);
+
   // beforeunload;
-  window.addEventListener('unload', () => {
-    localStorage.clear();
-  });
+  // window.addEventListener('unload', () => {
+  //   localStorage.clear();
+  // });
+
+  const viewSession = async () => {
+    const session = await getSession();
+    if (session === null) {
+      signOut();
+    } else {
+      update();
+    }
+  };
 
   return (
-    <Modals msgModal={`Your session is about to expire. Do you want to continue using your App?`} showModal={showModal}>
-      <Button
-        variant='contained'
-        onClick={() => {
-          setInactive(false);
-          setShowModal(false);
-          update();
-        }}
+    <>
+      <Button sx={{ display: 'none' }} ref={ref} onClick={() => viewSession()} />
+
+      <Modals
+        msgModal={`Your session is about to expire. Do you want to continue using your App?`}
+        showModal={showModal}
       >
-        {t('buttons.accept')}
-      </Button>
-    </Modals>
+        <Button
+          variant="contained"
+          onClick={() => {
+            setShowModal(false);
+            setInactive(false);
+          }}
+        >
+          {t('buttons.accept')}
+        </Button>
+      </Modals>
+    </>
   );
 }
